16866115d0090626f7d8e60782b03acdf1755a3a
dvizhokS <31057003+dvizhokS@users.noreply.github.com>
2019-09-26 16:44:42 +0300

    next

--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
 <?php
 
 require_once dirname(__DIR__).'/config/init.php';
+require_once LIBS.'/functions.php';
+
+new \ishop\App();
+
+\ishop\App::$app->setProperty("test", "Test");  
+debug(\ishop\App::$app->getProprties());
---
+++ vendor/ishop/core/Registry.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace ishop;
+
+class Registry{
+
+    use TSingletone;
+
+    public static $properties = [];
+
+    public function setProperty($name, $value){
+        self::$properties[$name] = $value;
+    }
+
+    public function getProperty($name){
+        if(isset(self::$properties[$name])){
+            return self::$properties[$name];
+        }
+        return null;
+    }
+
+    public function getProprties(){
+        return self::$properties;
+    }
+}
