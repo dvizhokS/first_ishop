e55e088b20f2975b38427b37dd80d25c6ea45852
DvizhokS <seregashos@gmail.com>
2019-10-04 16:18:50 +0300

    add widgets category part 1

--- app/controllers/AppController.php
+++ app/controllers/AppController.php
@@ -6,6 +6,7 @@ use app\models\AppModel;
 use app\widgets\currency\Currency;
 use ishop\base\Controller;
 use ishop\App;
+use ishop\Cache;
 
 class AppController extends Controller{
 
@@ -15,8 +16,20 @@ class AppController extends Controller{
         new AppModel();
         App::$app->setProperty('currencies', Currency::getCurrencies());
         App::$app->setProperty('currency', Currency::getCurrency(App::$app->getProperty('currencies')));
+        App::$app->setProperty('cats', self::cacheCategory());
+        // debug(App::$app->getProperties());
         
-        
+    }
+
+    public static function cacheCategory(){
+        $cache = Cache::instance();
+        $cats = $cache->get('cats');
+        if(!$cats){
+            $cats = \R::getAssoc("SELECT * FROM category");
+            $cache->set('cats', $cats);
+        }
+        return $cats;
+
     }
 
 }
