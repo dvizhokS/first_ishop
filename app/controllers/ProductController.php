a1ce463b2fe21d3f5836789b7780db1a7e57ca4f
DvizhokS <seregashos@gmail.com>
2019-10-07 23:54:15 +0300

    add gallery card

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -18,8 +18,11 @@ class ProductController extends AppController{
         $related = \R::getAll("SELECT * FROM related_product JOIN product ON 
 product.id = related_product.related_id WHERE related_product.product_id = ?",
 [$product->id]);
+
+        $gallery = \R::findAll('gallery', 'product_id = ?', [$product->id]);
+        
         $this->setMeta($product->title, $product->description, $product->keywords);
-        $this->set(compact('product', 'related'));
+        $this->set(compact('product', 'related','gallery'));
         
     
     }
