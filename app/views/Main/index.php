de07bab031a87e152c7a8dbce6c5beb2b732ff21
DvizhokS <seregashos@gmail.com>
2019-10-10 11:45:55 +0300

    add to cart part 1

--- app/views/Main/index.php
+++ app/views/Main/index.php
@@ -52,7 +52,7 @@
 							<div class="product-bottom">
 								<h3><a href="product/<?=$hit->alias; ?>"><?=$hit->title;?></h3>
 								<p>Explore Now</p>
-                                <h4><a class="add-to-cart-link" href="cart/add?=<?=$hit->id;?>"><i></i></a> <span class=" item_price"><?=$curr['symbol_left'];?><?=$hit->price * $curr['value'];?><?=$curr['symbol_right'];?></span>
+                                <h4><a data-id="<?=$hit->id;?>" class="add-to-cart-link" href="cart/add?=<?=$hit->id;?>"><i></i></a> <span class=" item_price"><?=$curr['symbol_left'];?><?=$hit->price * $curr['value'];?><?=$curr['symbol_right'];?></span>
                                     <?php if($hit->old_price): ?>
                                     <small style="color:red;"><del><?=$curr['symbol_left'];?><?=$hit->old_price * $curr['value'];?><?=$curr['symbol_right'];?></del></small>
                                     <?php endif; ?>
