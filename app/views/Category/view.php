3803aef6462f88a018b975691a363ace707d8af0
DvizhokS <seregashos@gmail.com>
2019-10-22 14:06:24 +0300

    add Pagination

--- app/views/Category/view.php
+++ app/views/Category/view.php
@@ -17,8 +17,8 @@
 				<div class="col-md-9 prdt-left">
                     <?php if(!empty($products)):?>    
                         <div class="product-one">
-                            <?php $curr = \ishop\App::$app->getProperty('currency');?>
-                            <?php foreach($products as $product): ?>
+							<?php $curr = \ishop\App::$app->getProperty('currency');?>
+							<?php foreach($products as $product): ?>
                             <div class="col-md-4 product-left p-left">
                                 <div class="product-main simpleCart_shelfItem">
                                     <a href="<?=$product->alias;?>" class="mask"><img class="img-responsive zoom-img" src="images/<?=$product->img;?>" alt="" /></a>
@@ -37,7 +37,13 @@
                                 </div>
                             </div>
                             <?php endforeach; ?>
-                            <div class="clearfix"></div>
+							<div class="clearfix"></div>
+							<div class="text-center">
+								<p>(<?=count($products)?> товара(ов) из <?=$total;?>)</p>
+								<?php if($pagination->countPages > 1): ?>
+									<?=$pagination;?>
+								<?php endif; ?>
+							</div>
                         </div>
                     <?php else: ?>
                         <h3>В этой категории товаров пока нет...</h3>
--- vendor/ishop/core/libs/Pagination.php
+++ vendor/ishop/core/libs/Pagination.php
@@ -9,16 +9,7 @@ class Pagination{
     public $total;
     public $countPages;
     public $uri;
-    public $links = [
-        'back' => '',
-        'forward' => '',
-        'startpage' => '',
-        'endpage' => '',
-        'page2left' => '',
-        'page1left' => '',
-        'page2right' => '',
-        'page1right' => '',
-    ];
+
 
     public function __construct($page, $perpage, $total){
         $this->perpage = $perpage;
@@ -26,7 +17,62 @@ class Pagination{
         $this->countPages = $this->getCountPages();
         $this->currentPage = $this->getCurrentPage($page);
         $this->uri = $this->getParams();
-        var_dump($this->uri);
+        // var_dump($this->uri);
+    }
+
+
+    public function getHtml(){
+        $back = null;
+        $forward = null;
+        $startpage = null;
+        $endpage = null;
+        $page2left = null;
+        $page1left = null;
+        $page2right = null;
+        $page1right = null;
+
+        if($this->currentPage > 1){
+            $back = "<li><a class='nav-link' href='{$this->uri}page=" . 
+                    ($this->currentPage - 1) . "'>&lt;</a></li>";
+        }
+        if($this->currentPage < $this->countPages){
+            $forward = "<li><a class='nav-link' href='{$this->uri}page=" . 
+                    ($this->currentPage + 1) . "'>&gt;</a></li>";
+        }
+        if($this->currentPage > 3){
+            $startpage = "<li><a class='nav-link' href='{$this->uri}page=1" .
+                        "'>&laquo;</a></li>";
+        }
+        if($this->currentPage < ($this->countPages - 2)){
+            $endpage = "<li><a class='nav-link' href='{$this->uri}page=($this->countPages)".
+                        "'>&raquo;</a></li>";
+        }
+        if($this->currentPage - 2 > 0){
+            $page2left = "<li><a class='nav-link' href='{$this->uri}page=" .
+                        ($this->currentPage - 2) ."'>" . ($this->currentPage - 2) . "</a></li>";
+        }
+        if($this->currentPage - 1 > 0){
+            $page1left = "<li><a class='nav-link' href='{$this->uri}page=" .
+                        ($this->currentPage - 1) ."'>" . ($this->currentPage - 1) . "</a></li>";
+        }
+        if($this->currentPage + 2 <= $this->countPages){
+            $page2right = "<li><a class='nav-link' href='{$this->uri}page=" .
+                        ($this->currentPage + 2) ."'>" . ($this->currentPage + 2) . "</a></li>";
+        }
+        if($this->currentPage + 1 <= $this->countPages){
+            $page1right = "<li><a class='nav-link' href='{$this->uri}page=" .
+                        ($this->currentPage + 1) ."'>" . ($this->currentPage + 1) . "</a></li>";
+        }
+
+        return '<ul class="pagination">' . $startpage . $back . $page2left . $page1left . 
+                '<li class = "active"><a>' . $this->currentPage . '</a></li>' . $page1right . 
+                $page2right . $forward. $endpage. '</ul>';
+
+    }
+
+    public function __toString()
+    {
+        return $this->getHtml();
     }
 
     public function getCountPages(){
@@ -49,9 +95,11 @@ class Pagination{
         $uri = $url[0] . '?';
         if(isset($url[1]) && $url[1] != ''){
             $params = explode('&', $url[1]);
-            debug($params);
+            foreach($params as $param){
+                if(!preg_match("#page=#", $param))  $uri .= "{$param}&amp;";
+            }
         }
-        return $url;
+        return urldecode($uri);
     }
     
 
