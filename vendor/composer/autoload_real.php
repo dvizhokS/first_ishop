779ef82103b1a3a10e8f232da54b1adadd04fd1b
DvizhokS <seregashos@gmail.com>
2019-10-24 14:56:39 +0300

    add order part 3

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit90912a242f8d4ca9f941462c4b26b80d
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit90912a242f8d4ca9f941462c4b26b80d::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire90912a242f8d4ca9f941462c4b26b80d($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire90912a242f8d4ca9f941462c4b26b80d($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
