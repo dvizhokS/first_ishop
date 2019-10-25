a836fb32225c555fd65e470e86a24af8e7418289
DvizhokS <seregashos@gmail.com>
2019-10-25 11:20:23 +0300

    test 2

---
+++ config/params.php
@@ -0,0 +1,13 @@
+<?php
+test...
+return [
+    'admin_email' => 'admin@mail.com',
+    'shop_name' => 'Магазин ishop.loc',
+    'pagination' => 3,
+    'smtp_host' => '',
+    'smtp_port' => '',
+    'smtp_protocol' => '',
+    'smtp_login' => '',
+    'smtp_password' => ''
+    
+];
