d28db3671248a71f231d5f3bd3a49c180ebe508f
DvizhokS <seregashos@gmail.com>
2019-10-29 20:47:29 +0200

    add filters part 2

--- app/widgets/filter/filter_tpl.php
+++ app/widgets/filter/filter_tpl.php
@@ -3,7 +3,7 @@
         <h4><?php $group_item; ?></h4>
         <div class="row1 scroll-pane">
             <div class="col col-4">								
-                <?php foreach($this->attrs[$group_id] as $arrr_id => $value): ?>
+                <?php foreach($this->attrs[$group_id] as $attr_id => $value): ?>
                 <label class="checkbox">
                     <input type="checkbox" name="checkbox" value="<?=$attr_id;?>"><i></i><?=$value;?>
                 </label>
