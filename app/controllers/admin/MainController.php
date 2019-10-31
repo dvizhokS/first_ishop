4428d5ba542153349e98bc621b5aa2d6dd500ca7
DvizhokS <seregashos@gmail.com>
2019-10-31 14:20:10 +0200

    add admin registration

--- app/controllers/admin/MainController.php
+++ app/controllers/admin/MainController.php
@@ -6,7 +6,7 @@ namespace app\controllers\admin;
 class MainController extends AppController{
 
     public function indexAction(){
-        
+        $this->setMeta('Панель управления');
     }
 
 }
